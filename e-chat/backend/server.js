7912d6ead7f743efb1b9b26592acc69f30e74238
Dustin Johnson <jdustin@babbage.cs.pdx.edu>
2021-03-10 13:27:25 -0800

    updates

--- e-chat/backend/server.js
+++ e-chat/backend/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
-const app = require('express')();
-const port = process.env.PORT || 5000;
 
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology: true}
@@ -19,10 +18,12 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-app.get('/', (req, res) => {
-  res.send('hey');
-});
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
-});
+const chatsRouter = require('./routes/chats');
+const usersRouter = require('./routes/users');
 
+app.use('/chats', chatsRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
--- e-chat/src/App.js
+++ e-chat/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/navbar.component";
+import ChatList from "./components/chat-list.component";
+import OpenChat from "./components/open-chat.component";
+import CreateChat from "./components/create-chat.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+     <div className="container">
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ChatList} />
+      <Route path="/edit/:id" component={OpenChat} />
+      <Route path="/create" component={CreateChat} />
+      <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
